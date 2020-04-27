3a8228b732b3f7e074d2d48b5c2af2f0180bd61e
soukupb <bar.soukupova@gmail.com>
2020-04-27 17:57:03 +0200

    udělá obrázek z textu

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
+from PIL import Image, ImageDraw, ImageFont
+import os
+
+
+def text_on_img(filename='01.png', text="Hello", size=12):
+    "Draw a text on an Image, saves it, show it"
+    fnt = ImageFont.truetype('arial.ttf', size)
+    # create image
+    image = Image.new(mode="RGB", size=(int(size / 2) * len(text), size + 50), color="red")
+    draw = ImageDraw.Draw(image)
+    # draw text
+    draw.text((10, 10), text, font=fnt, fill=(255, 255, 0))
+    # save file
+    image.save(filename)
+    # show file
+    os.system(filename)
+
+
 if __name__== "__main__":
-    print("Gello world")
+    text_on_img(text="Text to write on img", size=300)
