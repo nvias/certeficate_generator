7100b98f2f063157332f94b81fa72bdfb3ef5184
soukupb <bar.soukupova@gmail.com>
2020-04-28 00:44:22 +0200

    Funguje pro libovolne dlouhe jmeno, snad i uklizene vicemene, jsem super, diky D.<3

--- main.py
+++ main.py
@@ -1,42 +1,89 @@
 from PIL import Image, ImageDraw, ImageFont
 import os
 
+"""
+:param text
+ writes normal-length name in picture
+"""
 
-"""
-:param text,image,font
- writes text to console
-"""
-def certificat(text):
-    # nacte obrazek
+def normal_name(text):
     image = Image.open('certifikat.png')
     draw = ImageDraw.Draw(image)
-    # create font object with the font file and specify
-    # desired size
     size = 45
+    W = image.width
+    H = image.height
     font = ImageFont.truetype("arial.ttf", size=size)
-    # draw the message on the background
     w, h = draw.textsize(text, font=font)
-    text = text.split(" ")
-
-    if text.length >= 3:
-        text1 = text [0] + text [1]
-        for (i = 3;i<=text.lenght-1;i+=1):
-            text2 = text2 +text [i]
-
-    print(text)
     while w > 550:
-        size = size-1
+        size = size - 1
         font = ImageFont.truetype("arial.ttf", size=size)
         w, h = draw.textsize(text, font=font)
 
-    W = image.width
-    H = image.height
     posH = (H - h) / 2 + 145
     posW = (W / 2) - 0.5 * w
     draw.text((posW, posH), text, fill="black", font=font)
-    # save the edited image
-    image.save('certificat_'+text+'.png') #for testing purposes
+    image.save('certificat_' + text + '.png')  # for testing purposes
+    return image
+"""
+:param text
+ writes long name on picture
+"""
+def long_name(text1):
+    image = Image.open('certifikat.png')
+    draw = ImageDraw.Draw(image)
+    size = 45
+    W = image.width
+    H = image.height
+    font = ImageFont.truetype("arial.ttf", size=size)
+    jmeno = text1[0] + " " + text1[1]
+    prijmeni = text1[2]
+    if len(text1) > 3:
+        prijmeni = text1[2] + " " + text1[3]
+
+    wj, hj = draw.textsize(jmeno, font=font)
+    wp, hp = draw.textsize(prijmeni, font=font)
+    size1 = 45
+    size2 = 45
+    while wj > 550:
+        size1 = size1 - 1
+        font = ImageFont.truetype("arial.ttf", size=size1)
+        wj, hj = draw.textsize(jmeno, font=font)
+
+    while wp > 550:
+        size2 = size2 - 1
+        font = ImageFont.truetype("arial.ttf", size=size2)
+        wp, hp = draw.textsize(prijmeni, font=font)
+
+    posHj = (H - hj) / 2 + 145 - 0.75 * hj
+    posWj = (W / 2) - 0.5 * wj
+    posHp = (H - hp) / 2 + 145 + 0.75 * hp
+    posWp = (W / 2) - 0.5 * wp
+    draw.text((posWj, posHj), jmeno, fill="black", font=font)
+    draw.text((posWp, posHp), prijmeni, fill="black", font=font)
+    image.save('certificat_' + jmeno + " " + prijmeni + '.png')  # for testing purposes
     return image
 
+"""
+:param text
+ writes text on picture
+"""
 
-certificat("Katarina-Anna Marie Soukupova")
+def certificat(text):
+    image = Image.open('certifikat.png')
+    draw = ImageDraw.Draw(image)
+    size = 45
+    W = image.width
+    H = image.height
+    font = ImageFont.truetype("arial.ttf", size=size)
+    text1 = text.split(" ")
+    if len(text1) > 2:
+        long_name(text1)
+
+    else:
+        normal_name(text)
+
+
+
+
+
+certificat("Katarina-Anna Marie")
