5bc9295382e7017b594468467ba2d81dc76984eb
soukupb <bar.soukupova@gmail.com>
2020-04-27 23:20:02 +0200

    ok

--- main.py
+++ main.py
@@ -12,17 +12,31 @@ def certificat(text):
     draw = ImageDraw.Draw(image)
     # create font object with the font file and specify
     # desired size
-    font = ImageFont.truetype("arial.ttf", size=45)
+    size = 45
+    font = ImageFont.truetype("arial.ttf", size=size)
     # draw the message on the background
+    w, h = draw.textsize(text, font=font)
+    text = text.split(" ")
+
+    if text.length >= 3:
+        text1 = text [0] + text [1]
+        for (i = 3;i<=text.lenght-1;i+=1):
+            text2 = text2 +text [i]
+
+    print(text)
+    while w > 550:
+        size = size-1
+        font = ImageFont.truetype("arial.ttf", size=size)
+        w, h = draw.textsize(text, font=font)
+
     W = image.width
     H = image.height
-    w, h = draw.textsize(text, font=font)
     posH = (H - h) / 2 + 145
     posW = (W / 2) - 0.5 * w
     draw.text((posW, posH), text, fill="black", font=font)
     # save the edited image
-    image.save('certificat_'+text+'.png')
+    image.save('certificat_'+text+'.png') #for testing purposes
     return image
 
 
-certificat("Barbora Soukupova")
+certificat("Katarina-Anna Marie Soukupova")
