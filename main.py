bb2aa7423853e00001d77766820d38ee421f8ad4
soukupb <bar.soukupova@gmail.com>
2020-04-27 22:06:33 +0200

    uklizeno

--- main.py
+++ main.py
@@ -6,24 +6,22 @@ import os
 :param text,image,font
  writes text to console
 """
-def certificat(text,image,font):
+def certificat(text):
     # nacte obrazek
-    image = Image.open(image)
+    image = Image.open('certifikat.png')
     draw = ImageDraw.Draw(image)
     # create font object with the font file and specify
     # desired size
-    font = ImageFont.truetype(font, size=45)
-    msg = text
-    color = 'rgb(0, 0, 0)'  # black color
+    font = ImageFont.truetype("arial.ttf", size=45)
     # draw the message on the background
     W = image.width
     H = image.height
-    w, h = draw.textsize(msg, font=font)
+    w, h = draw.textsize(text, font=font)
     posH = (H - h) / 2 + 145
     posW = (W / 2) - 0.5 * w
-    draw.text((posW, posH), msg, fill="black", font=font)
+    draw.text((posW, posH), text, fill="black", font=font)
     # save the edited image
-    image.save('greeting_card.png')
+    image.save('certificat_'+text+'.png')
 
 
-certificat("Barbora Soukupova", 'certifikat.png', 'arial.ttf')
+certificat("Barbora Soukupova")
