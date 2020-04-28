691ceff142b00ce96d2029777add1f182f3ea521
soukupb <bar.soukupova@gmail.com>
2020-04-28 17:05:01 +0200

    done

--- main.py
+++ main.py
@@ -6,35 +6,40 @@ import os
  writes normal-length name in picture
 """
 
-def normal_name(text):
+
+def normal_name(text, size):
     image = Image.open('certifikat.png')
     draw = ImageDraw.Draw(image)
-    size = 45
+    size = size
     W = image.width
     H = image.height
-    font = ImageFont.truetype("arial.ttf", size=size)
+    font = ImageFont.truetype("fonts\\introhead.otf", size=size)
     w, h = draw.textsize(text, font=font)
     while w > 550:
         size = size - 1
-        font = ImageFont.truetype("arial.ttf", size=size)
+        font = ImageFont.truetype("fonts\\introhead.otf", size=size)
         w, h = draw.textsize(text, font=font)
 
-    posH = (H - h) / 2 + 145
+    posH = (H - h) / 2 - 30
     posW = (W / 2) - 0.5 * w
     draw.text((posW, posH), text, fill="black", font=font)
     image.save('certificat_' + text + '.png')  # for testing purposes
     return image
+
+
 """
 :param text
  writes long name on picture
 """
-def long_name(text1):
+
+
+def long_name(text1, size):
     image = Image.open('certifikat.png')
     draw = ImageDraw.Draw(image)
-    size = 45
+    size = size
     W = image.width
     H = image.height
-    font = ImageFont.truetype("arial.ttf", size=size)
+    font = ImageFont.truetype("fonts\\introhead.otf", size=size)
     jmeno = text1[0] + " " + text1[1]
     prijmeni = text1[2]
     if len(text1) > 3:
@@ -42,48 +47,46 @@ def long_name(text1):
 
     wj, hj = draw.textsize(jmeno, font=font)
     wp, hp = draw.textsize(prijmeni, font=font)
-    size1 = 45
-    size2 = 45
+    size1 = size
+    size2 = size
     while wj > 550:
         size1 = size1 - 1
-        font = ImageFont.truetype("arial.ttf", size=size1)
+        font = ImageFont.truetype("fonts\\introhead.otf", size=size1)
         wj, hj = draw.textsize(jmeno, font=font)
 
     while wp > 550:
         size2 = size2 - 1
-        font = ImageFont.truetype("arial.ttf", size=size2)
+        font = ImageFont.truetype("fonts\\introhead.otf", size=size2)
         wp, hp = draw.textsize(prijmeni, font=font)
 
-    posHj = (H - hj) / 2 + 145 - 0.75 * hj
+    posHj = (H - hj) / 2 - 0.75 * hj - 30
     posWj = (W / 2) - 0.5 * wj
-    posHp = (H - hp) / 2 + 145 + 0.75 * hp
+    posHp = (H - hp) / 2 + 0.75 * hp - 30
     posWp = (W / 2) - 0.5 * wp
     draw.text((posWj, posHj), jmeno, fill="black", font=font)
     draw.text((posWp, posHp), prijmeni, fill="black", font=font)
     image.save('certificat_' + jmeno + " " + prijmeni + '.png')  # for testing purposes
     return image
 
+
 """
 :param text
  writes text on picture
 """
 
-def certificat(text):
+
+def certificat(text, size):
     image = Image.open('certifikat.png')
     draw = ImageDraw.Draw(image)
-    size = 45
     W = image.width
     H = image.height
-    font = ImageFont.truetype("arial.ttf", size=size)
+    font = ImageFont.truetype("fonts\\introhead.otf", size=size)
     text1 = text.split(" ")
     if len(text1) > 2:
-        long_name(text1)
+        long_name(text1, size)
 
     else:
-        normal_name(text)
+        normal_name(text, size)
 
 
-
-
-
-certificat("Katarina-Anna Marie")
+certificat("Jmeno Prijmeni", 55)
