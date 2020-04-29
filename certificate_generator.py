0d4a0a17d2d2e3ce712ab7c33d8656b4f52e8447
David Žahour <davidzah6@seznam.cz>
2020-04-30 01:02:09 +0200

    ropzracované opravy báry kódu

--- certificate_generator.py
+++ certificate_generator.py
@@ -1,8 +1,8 @@
-from pathlib import Path
-from PIL import Image, ImageDraw, ImageFont
-
 import os
+import time
+from pathlib import Path
 
+from PIL import Image, ImageDraw, ImageFont
 
 """
 :param text
@@ -11,19 +11,26 @@ import os
 
 
 def normal_name(text, size, image):
+    start_time = time.time()
+
     draw = ImageDraw.Draw(image)
     W = image.width
     H = image.height
     w, h = draw.textsize(text, font=font(size))
+
+    print("--- %s seconds ---" % (time.time() - start_time))
     while w > 2300:
         size = size - 1
         w, h = draw.textsize(text, font=font(size))
+    print("while --- %s seconds ---" % (time.time() - start_time))
 
     posH = (H - h) / 2 + 30
     posW = (W / 2) - 0.5 * w
     draw.text((posW, posH), text, fill="black", font=font(size))
-    path = Path("certificates/" + "certificat_" + text + ".png")
-    image.save(path.as_posix())  # for testing purposes
+    if __debug__:
+        path = Path("certificates/" + "certificat_" + text + ".png")
+        image.save(path.as_posix())  # for testing purposes
+    print(" end --- %s seconds ---" % (time.time() - start_time))
     return image
 
 
@@ -34,6 +41,7 @@ def normal_name(text, size, image):
 
 
 def long_name(text1, size, image):
+    print("--- %s seconds ---" % (time.time() - start_time))
     draw = ImageDraw.Draw(image)
     size1 = size
     size2 = size
@@ -44,24 +52,34 @@ def long_name(text1, size, image):
     if len(text1) > 3:
         prijmeni = text1[2] + " " + text1[3]
 
-    w1, h1 = draw.textsize(jmeno, font=font(size1))
+    wj, hj = draw.textsize(jmeno, font=font(size1))
     wp, hp = draw.textsize(prijmeni, font=font(size2))
-    while w1 > 2300:
+    print("--- %s seconds ---" % (time.time() - start_time))
+    while wj > 2300:
         size1 = size1 - 1
-        w1, h1 = draw.textsize(jmeno, font=font(size1))
-    while w2 > 2300:
+        wj, hj = draw.textsize(jmeno, font=font(size1))
+    while wp > 2300:
         size2 = size2 - 1
-        w2, h2 = draw.textsize(prijmeni, font=font(size2))
-
-    posH1 = (H - h1) / 2 - 0.5 * h1 + 70
-    posH2 = (H - h2) / 2 + 0.5 * h2 + 70
-    posW1 = (W / 2) - 0.5 * w1
-    posW2 = (W / 2) - 0.5 * wp
-    draw.text((posW1, posH1), jmeno, fill="black", font=font(size1))
-    draw.text((posW1, posH2), prijmeni, fill="black", font=font(size2))
-    path = Path("certificates/" + "certificat_" + jmeno + "_" + prijmeni + ".png")
-    image.save(path.as_posix())  # for testing purposes
+        wp, hp = draw.textsize(prijmeni, font=font(size2))
+    print("--- %s seconds ---" % (time.time() - start_time))
+    posHj = (H - hj) / 2 - 0.5 * hj + 70
+    posHp = (H - hp) / 2 + 0.5 * hp + 70
+    posWj = (W / 2) - 0.5 * wj
+    posWp = (W / 2) - 0.5 * wp
+    draw.text((posWj, posHj), jmeno, fill="black", font=font(size1))
+    draw.text((posWp, posHp), prijmeni, fill="black", font=font(size2))
+    if __debug__:
+        path = Path("certificates/" + "certificat_" + jmeno + "_" + prijmeni + ".png")
+        image.save(path.as_posix())  # for testing purposes
     return image
+
+
+def font(size):
+    path = Path("fonts/introhead.otf")
+    path = path.as_posix()
+
+    font = ImageFont.truetype(path, size=size)
+    return font
 
 
 """
@@ -70,21 +88,24 @@ def long_name(text1, size, image):
 """
 
 
-def certificat(text, size, image):
+def certificat(text, size=300, image='certifikatAI.png'):
+    image = Image.open(image)
+    path = Path("certificates/")
+    path = path.as_posix()
+
+    if not os.path.exists(path):
+        os.makedirs(path)
+
     text1 = text.split(" ")
     if len(text1) > 2:
-        long_name(text1, size, image)
+        img = long_name(text1, size, image)
 
     else:
-        normal_name(text, size, image)
-
-def font(size):
-    path = Path("fonts/introhead.otf")
-    path = path.as_posix()
-    font = ImageFont.truetype(path, size=size)
-    return font
+        img = normal_name(text, size, image)
+    return img
 
 
-image = Image.open('certifikatAI.png')
-size = 300
-certificat("Zadej Jmeno", size, image)
+if __name__ == "__main__":
+    image = Image.open('certifikatAI.png')
+    size = 300
+    certificat("Zadej Jmeno", size, image)
