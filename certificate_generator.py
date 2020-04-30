cb31adcc55c45c336e6bfa4f32f6ea9a20da1ae6
soukupb <bar.soukupova@gmail.com>
2020-04-30 19:49:30 +0200

    updated

--- certificate_generator.py
+++ certificate_generator.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 from PIL import Image, ImageDraw, ImageFont
-
 import os
 
 
@@ -9,15 +8,19 @@ import os
  writes normal-length name in picture
 """
 
-
 def normal_name(text, size, image):
     draw = ImageDraw.Draw(image)
     W = image.width
     H = image.height
     w, h = draw.textsize(text, font=font(size))
-    while w > 2300:
-        size = size - 1
-        w, h = draw.textsize(text, font=font(size))
+    if w > 2300:
+        w_change = ((w + 2300) / 2300) * 0.65
+        size = round(size / w_change)
+    w, h = draw.textsize(text, font=font(size))
+
+   # while w > 2300:
+    #    size = size - 1
+     #   w, h = draw. textsize(text, font=font(size))
 
     posH = (H - h) / 2 + 30
     posW = (W / 2) - 0.5 * w
@@ -45,20 +48,28 @@ def long_name(text1, size, image):
         prijmeni = text1[2] + " " + text1[3]
 
     w1, h1 = draw.textsize(jmeno, font=font(size1))
-    wp, hp = draw.textsize(prijmeni, font=font(size2))
-    while w1 > 2300:
-        size1 = size1 - 1
-        w1, h1 = draw.textsize(jmeno, font=font(size1))
-    while w2 > 2300:
-        size2 = size2 - 1
-        w2, h2 = draw.textsize(prijmeni, font=font(size2))
+    w2, h2 = draw.textsize(prijmeni, font=font(size2))
+
+    if w1 > 2300:
+        w_change1 = ((w1 + 2300) / 2300) * 0.65
+        size1 = round(size1 / w_change1)
+
+    if w2 > 2300:
+        w_change2 = ((w2 + 2300) / 2300) * 0.65
+        size2 = round(size2 / w_change2)
+
+    w1, h1 = draw.textsize(jmeno, font=font(size1))
+    w2, h2 = draw.textsize(prijmeni, font=font(size2))
+
+
 
     posH1 = (H - h1) / 2 - 0.5 * h1 + 70
     posH2 = (H - h2) / 2 + 0.5 * h2 + 70
     posW1 = (W / 2) - 0.5 * w1
-    posW2 = (W / 2) - 0.5 * wp
+    posW2 = (W / 2) - 0.5 * w2
+
     draw.text((posW1, posH1), jmeno, fill="black", font=font(size1))
-    draw.text((posW1, posH2), prijmeni, fill="black", font=font(size2))
+    draw.text((posW2, posH2), prijmeni, fill="black", font=font(size2))
     path = Path("certificates/" + "certificat_" + jmeno + "_" + prijmeni + ".png")
     image.save(path.as_posix())  # for testing purposes
     return image
@@ -78,13 +89,17 @@ def certificat(text, size, image):
     else:
         normal_name(text, size, image)
 
+    return image
+
+
 def font(size):
     path = Path("fonts/introhead.otf")
     path = path.as_posix()
     font = ImageFont.truetype(path, size=size)
     return font
 
+if __name__ == '__main__':
 
-image = Image.open('certifikatAI.png')
-size = 300
-certificat("Zadej Jmeno", size, image)
+    image = Image.open('certifikatAI.png')
+    size = 300
+    certificat("Zadej Jmeno", size, image)
