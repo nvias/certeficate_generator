75390e963fc49acb03eb7676b600a7359ec06ca0
David Žahour <davidzah6@seznam.cz>
2020-05-01 16:59:16 +0200

    FINISH

--- certificate_generator.py
+++ certificate_generator.py
@@ -1,5 +1,4 @@
 import os
-import time
 from pathlib import Path
 
 from PIL import Image, ImageDraw, ImageFont
@@ -11,26 +10,26 @@ from PIL import Image, ImageDraw, ImageFont
 
 
 def normal_name(text, size, image):
-    start_time = time.time()
 
     draw = ImageDraw.Draw(image)
     W = image.width
     H = image.height
     w, h = draw.textsize(text, font=font(size))
 
-    print("--- %s seconds ---" % (time.time() - start_time))
+    counter = 0
     while w > 2300:
+        counter = counter + 1
         size = size - 1
         w, h = draw.textsize(text, font=font(size))
-    print("while --- %s seconds ---" % (time.time() - start_time))
 
     posH = (H - h) / 2 + 30
     posW = (W / 2) - 0.5 * w
+
     draw.text((posW, posH), text, fill="black", font=font(size))
-    if __debug__:
+    if not __debug__:
         path = Path("certificates/" + "certificat_" + text + ".png")
         image.save(path.as_posix())  # for testing purposes
-    print(" end --- %s seconds ---" % (time.time() - start_time))
+
     return image
 
 
@@ -41,7 +40,6 @@ def normal_name(text, size, image):
 
 
 def long_name(text1, size, image):
-    print("--- %s seconds ---" % (time.time() - start_time))
     draw = ImageDraw.Draw(image)
     size1 = size
     size2 = size
@@ -54,21 +52,21 @@ def long_name(text1, size, image):
 
     wj, hj = draw.textsize(jmeno, font=font(size1))
     wp, hp = draw.textsize(prijmeni, font=font(size2))
-    print("--- %s seconds ---" % (time.time() - start_time))
+
     while wj > 2300:
         size1 = size1 - 1
         wj, hj = draw.textsize(jmeno, font=font(size1))
     while wp > 2300:
         size2 = size2 - 1
         wp, hp = draw.textsize(prijmeni, font=font(size2))
-    print("--- %s seconds ---" % (time.time() - start_time))
+
     posHj = (H - hj) / 2 - 0.5 * hj + 70
     posHp = (H - hp) / 2 + 0.5 * hp + 70
     posWj = (W / 2) - 0.5 * wj
     posWp = (W / 2) - 0.5 * wp
     draw.text((posWj, posHj), jmeno, fill="black", font=font(size1))
     draw.text((posWp, posHp), prijmeni, fill="black", font=font(size2))
-    if __debug__:
+    if not __debug__:
         path = Path("certificates/" + "certificat_" + jmeno + "_" + prijmeni + ".png")
         image.save(path.as_posix())  # for testing purposes
     return image
@@ -106,6 +104,5 @@ def certificat(text, size=300, image='certifikatAI.png'):
 
 
 if __name__ == "__main__":
-    image = Image.open('certifikatAI.png')
     size = 300
-    certificat("Zadej Jmeno", size, image)
+    certificat("Dave")
