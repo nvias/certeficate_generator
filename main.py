d4c7effdc16e860be3eac4c1942bbc484fd30b4b
David Žahour <davidzah6@seznam.cz>
2020-06-22 20:50:37 +0200

    need change to single thread

--- main.py
+++ main.py
@@ -1,26 +1,65 @@
-import csv
+import argparse
 import os
+import sys
+import time
+from multiprocessing import Process
 from pathlib import Path
 
 from certificate_generator import certificat
 
 
 def load_names(file):
-    with open(file, mode='r', encoding="utf-8") as csv_file:
-        csv_reader = csv.DictReader(csv_file)
-        line_count = 0
+    with open(file, mode='r', encoding='utf-16') as csv_file:
+        # need fix
         list2 = [row.split()[0] for row in csv_file]
         print(list2)
         return list2
 
 
-if __name__ == "__main__":
-    list = load_names(r'C:\Users\David\Documents\list.csv')
+def create_img(name):
+    img = certificat(name)
+    path = Path("output\\" + name + ".png")
+    img.save(path.as_posix())  # for testing purposes
 
-    if not os.path.exists("\\output"):
-        os.makedirs("\\output")
+
+def load_credentials(file):
+    try:
+        with open(file) as f:
+            lines = f.readlines()
+            lines = [x.strip() for x in lines]
+    except Exception:
+        print('failed to read file ')
+        sys.exit(1)
+    return lines
+
+
+def wait():
+    blah = "\|/-\|/-"
+    for l in blah:
+        sys.stdout.write(l)
+        sys.stdout.flush()
+        sys.stdout.write('\b')
+        time.sleep(0.2)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--file')
+    args = parser.parse_args()
+
+    list = load_credentials(args.file)
+
+    if not os.path.exists("output"):
+        os.makedirs("output")
+
+    processes = []
 
     for x in list:
-        img = certificat(x)
-        path = Path("\\output\\" + "certificat_" + x + ".png")
-        img.save(path.as_posix())  # for testing purposes
+        p = Process(target=create_img, args=(x,))
+        p.start()
+        processes.append(p)
+
+    for p in processes:
+        p.join()
+
+    print("Done")
