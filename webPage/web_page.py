a2ec706355607330ed6f1beb49412d3679c3c6e6
David Žahour <davidzah6@seznam.cz>
2021-02-06 00:00:34 +0100

    Production FIX

--- webPage/web_page.py
+++ webPage/web_page.py
@@ -40,8 +40,8 @@ def hello_world():
 
 
 def run(port=8000):
-    app.run(debug=True, host='127.0.0.1', port=port)
+    app.run(debug=True, host='0.0.0.0', port=port)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
