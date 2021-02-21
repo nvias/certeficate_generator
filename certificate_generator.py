727e35d958335fd7f6ebea905f345d09377a4304
David Žahour <davidzah6@seznam.cz>
2021-02-21 16:21:17 +0100

    Add config and positinn stuff

--- certificate_generator.py
+++ certificate_generator.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 from PIL import Image, ImageDraw, ImageFont
@@ -7,19 +8,20 @@ from PIL import Image, ImageDraw, ImageFont
  writes normal-length name in picture
 """
 
-def normal_name(text, size, image):
+
+def normal_name(text, size, image, height=-530, bari_posrana_konstanta=2300, text_height=300):
     draw = ImageDraw.Draw(image)
     W = image.width
     H = image.height
     w, h = draw.textsize(text, font=font(size))
 
     counter = 0
-    while w > 2300:
+    while w > bari_posrana_konstanta or h > text_height:
         counter = counter + 1
         size = size - 1
         w, h = draw.textsize(text, font=font(size))
 
-    posH = (H - h) / 2 + 30
+    posH = (H - h) / 2 + height
     posW = (W / 2) - 0.5 * w
 
     draw.text((posW, posH), text, fill="black", font=font(size))
@@ -36,7 +38,7 @@ def normal_name(text, size, image):
 """
 
 
-def long_name(text1, size, image):
+def long_name(text1, size, image, height=-530, bari_posrana_konstanta=2300, text_height=300):
     draw = ImageDraw.Draw(image)
     size1 = size
     size2 = size
@@ -50,15 +52,15 @@ def long_name(text1, size, image):
     wj, hj = draw.textsize(jmeno, font=font(size1))
     wp, hp = draw.textsize(prijmeni, font=font(size2))
 
-    while wj > 2300:
+    while wj > bari_posrana_konstanta or hj > text_height:
         size1 = size1 - 1
         wj, hj = draw.textsize(jmeno, font=font(size1))
-    while wp > 2300:
+    while wp > bari_posrana_konstanta or hp > text_height:
         size2 = size2 - 1
         wp, hp = draw.textsize(prijmeni, font=font(size2))
 
-    posHj = (H - hj) / 2 - 0.5 * hj + 70
-    posHp = (H - hp) / 2 + 0.5 * hp + 70
+    posHj = (H - hj) / 2 - 0.5 * hj + height
+    posHp = (H - hp) / 2 + 0.5 * hp + height
     posWj = (W / 2) - 0.5 * wj
     posWp = (W / 2) - 0.5 * wp
     draw.text((posWj, posHj), jmeno, fill="black", font=font(size1))
@@ -83,18 +85,27 @@ def font(size):
 """
 
 
-def certificat(text, size=300,
-               image=r'C:\Users\David\PycharmProjects\certeficate_generator\certificates\certifikatAI2.png'):
-    image = Image.open(image)
+def certificat(text, size=300):
+    f = open("config", "r")
+    conf = f.readlines()
+    f.close()
+    conf = [item.strip() for item in conf]
+
+    image = Image.open(conf[0])
     path = Path("certificates/")
+    path = path.as_posix()
+
+    if not os.path.exists(path):
+        os.makedirs(path)
 
     text1 = text.split(" ")
-
     if len(text1) > 2:
-        img = long_name(text1, size, image)
+        img = long_name(text1, size, image, height=int(conf[2]), bari_posrana_konstanta=int(conf[3]),
+                        text_height=int(conf[4]))
 
     else:
-        img = normal_name(text, size, image)
+        img = normal_name(text, size, image, height=int(conf[2]), bari_posrana_konstanta=int(conf[3]),
+                          text_height=int(conf[4]))
     return img
 
 
