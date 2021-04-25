286db81f53610ea800a324b52d9b9c8e52474b2c
David <davidzah6@seznam.cz>
2021-04-25 22:05:34 +0200

    Add multiple config and arg parser

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from webPage.web_page import run
+import argparse
 
 if __name__ == "__main__":
-    f = open("config", "r")
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-c','--config',help="Config file by read me")
+    args = parser.parse_args()
+    f = 0
+    try:
+        f = open(args.config, "r")
+    except:
+        print("Config file not found")
+        raise
     conf = f.readlines()
     f.close()
     conf = [item.strip() for item in conf]
